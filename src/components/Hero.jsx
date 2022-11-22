c9f71e8fe99a83e0b2bfde8a688e2db7de87b7b9
= <=>
2022-11-23 00:09:14 +1000

    add Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import styles from "../style";
+import { discount, robot } from "../assets";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStarts} flex-col xl:px-0 sm:px-16 px-6`}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounder-[10px] mb-2">
+          <img src={discount} alt="discount" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
