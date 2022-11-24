ef594886005e1de9c62da9d9d815a0d97fd8ed37
Edward Duong <stt.snape@gmail.com>
2022-11-24 22:05:52 +1000

    add testimonial and clients

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles}`}
+      className={`font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
